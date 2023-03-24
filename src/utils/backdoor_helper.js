ffdc3b27a13540b6d3575e297dc76a1d4bbf83c7
Norbert Nadler <norbert.nadler@webpont.com>
2023-03-24 15:49:38 +0100

    extended backdoor_helper.js

--- src/utils/backdoor_helper.js
+++ src/utils/backdoor_helper.js
@@ -1,41 +1,47 @@
 import {Server} from '/utils/data/Server.js';
 import {FACTIONS_BACKDOOR_REQUIREMENTS} from "/constants/Factions";
 import {SERVER_NAME_HOME} from "/constants/ServerNames";
+import {ADDITIONAL_BACKDOORS} from "/constants/Misc";
 
 /** @param {NS} ns */
 export async function main(ns) {
     let homeServer = new Server(ns, SERVER_NAME_HOME);
 
-    // for (let targetHostName of FACTIONS_BACKDOOR_REQUIREMENTS) {
     for (let factionName of Object.keys(FACTIONS_BACKDOOR_REQUIREMENTS)) {
-        let targetHostName = FACTIONS_BACKDOOR_REQUIREMENTS[factionName];
-        let targetServer = searchForServer(homeServer, targetHostName);
-        if (targetServer.backdoorInstalled ) {
-            ns.tprintf('%s already backdoored.', targetServer.name);
-            continue;
-        } else if (!targetServer.hackable) {
-            ns.tprintf('%s can\'t be hacked yet. Requires hacking level of: %s', targetServer.name, targetServer.requiredHackingLevel);
-            continue;
-        } else if (!targetServer.hasRootAccess) {
-            ns.tprintf('%s doesn\'t have root access yet.', targetServer.name);
-            continue;
-        }
-        ns.tprintf('%s can and should be backdoored.', targetServer.name);
-
-        let flatArray = [];
-
-        gatherAncestry(homeServer, targetServer.name, flatArray);
-        flatArray.shift();
-
-        ns.tprintf('------------------------');
-        ns.tprintf('%s;', SERVER_NAME_HOME);
-        for (let server of flatArray) {
-            ns.tprintf('connect %s;', server);
-        }
-        ns.tprintf('backdoor');
-        ns.tprintf('------------------------');
-
+        generateBackdoorInstructions(ns, homeServer, FACTIONS_BACKDOOR_REQUIREMENTS[factionName]);
     }
+    for (let targetHostName of ADDITIONAL_BACKDOORS) {
+        generateBackdoorInstructions(ns, homeServer, targetHostName);
+    }
+    ns.alert('WORLD DAEMON NOT IMPLEMENTED YET');
+}
+
+function generateBackdoorInstructions(ns, homeServer, targetHostName) {
+    let targetServer = searchForServer(homeServer, targetHostName);
+    if (targetServer.backdoorInstalled) {
+        ns.tprintf('%s already backdoored.', targetServer.name);
+        return;
+    } else if (!targetServer.hackable) {
+        ns.tprintf('%s can\'t be hacked yet. Requires hacking level of: %s', targetServer.name, targetServer.requiredHackingLevel);
+        return;
+    } else if (!targetServer.hasRootAccess) {
+        ns.tprintf('%s doesn\'t have root access yet.', targetServer.name);
+        return;
+    }
+    ns.tprintf('%s can and should be backdoored.', targetServer.name);
+
+    let flatArray = [];
+
+    gatherAncestry(homeServer, targetServer.name, flatArray);
+    flatArray.shift();
+
+    ns.tprintf('------------------------');
+    ns.tprintf('%s;', SERVER_NAME_HOME);
+    for (let server of flatArray) {
+        ns.tprintf('connect %s;', server);
+    }
+    ns.tprintf('backdoor');
+    ns.tprintf('------------------------');
 }
 
 /**
